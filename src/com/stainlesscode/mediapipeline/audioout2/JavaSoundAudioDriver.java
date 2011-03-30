f4ed13d0ba04d1938b2e9c7041c9fee11751c0d3
dstieglitz@stainlesscode.com <dstieglitz@stainlesscode.com@550cbc61-9006-8ef8-ba6c-9951c14b7cef>
2011-03-30 16:28:43 +0000

    cleaned up audio sync

--- src/com/stainlesscode/mediapipeline/audioout2/JavaSoundAudioDriver.java
+++ src/com/stainlesscode/mediapipeline/audioout2/JavaSoundAudioDriver.java
@@ -32,7 +32,9 @@ import org.slf4j.LoggerFactory;
 
 import com.stainlesscode.mediapipeline.AudioOutput2;
 import com.stainlesscode.mediapipeline.EngineRuntime;
-import com.Ostermiller.util.CircularByteBuffer;
+import com.stainlesscode.mediapipeline.event.MediaPlayerEvent;
+import com.stainlesscode.mediapipeline.event.MediaPlayerEventListener;
+import com.stainlesscode.mediapipeline.sync.MultispeedVptsSynchronizer;
 import com.stainlesscode.mediapipeline.util.TimeUtil;
 import com.xuggle.xuggler.IAudioSamples;
 
@@ -40,26 +42,23 @@ import com.xuggle.xuggler.IAudioSamples;
  * @author Dan Stieglitz
  * 
  */
-public class JavaSoundAudioDriver implements AudioOutput2 {
-
-	private static final int BUFFER_SIZE_IN_BYTES = 131072;
+public class JavaSoundAudioDriver implements AudioOutput2,
+		MediaPlayerEventListener {
 
 	private SourceDataLine line = null;
 	private AudioFormat format;
-	private long lastPts;
 	private EngineRuntime engineRuntime;
-	private CircularByteBuffer buf;
-	// private long audioWriteLatency;
-	private byte[] bbuf;
-	private int bufferSizeInFrames;
-	private int off;
-	private int len;
+	private AudioBuffer buf;
+	private int bufferSizeInFrames = 96000;
+	private long firstTimestampInStream = -1;
 
 	private static Logger LogUtil = LoggerFactory
 			.getLogger(JavaSoundAudioDriver.class);
 
 	@Override
 	public void close() {
+		firstTimestampInStream = -1;
+		buf.clear();
 		line.close();
 	}
 
@@ -70,16 +69,22 @@ public class JavaSoundAudioDriver implements AudioOutput2 {
 
 	@Override
 	public long getLastPts() {
-		return lastPts;
+		return buf.getStartTimestampMillis() * 1000;
 	}
 
 	@Override
-	public void init(EngineRuntime engineRuntime, AudioFormat format) {
+	public void init(EngineRuntime engineRuntime, final AudioFormat format) {
 		this.engineRuntime = engineRuntime;
 		this.format = format;
 		DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-		buf = new CircularByteBuffer(BUFFER_SIZE_IN_BYTES, true);
-		bufferSizeInFrames = BUFFER_SIZE_IN_BYTES / format.getFrameSize();
+		buf = new AudioBuffer(format, bufferSizeInFrames
+				* format.getFrameSize(), true);
+
+		if (LogUtil.isDebugEnabled()) {
+			LogUtil.debug("frameSize=" + format.getFrameSize());
+			LogUtil.debug("buf.size=" + buf.getSize());
+			LogUtil.debug("bufTime=" + buf.getBufferTime());
+		}
 
 		try {
 			line = (SourceDataLine) AudioSystem.getLine(info);
@@ -91,9 +96,10 @@ public class JavaSoundAudioDriver implements AudioOutput2 {
 
 		Thread thread = new Thread(new Runnable() {
 
+			private long audioWriteLatency;
+
 			@Override
 			public void run() {
-				// FIXME BAD
 				while (true) {
 					if (JavaSoundAudioDriver.this.engineRuntime.isPaused())
 						continue;
@@ -101,179 +107,173 @@ public class JavaSoundAudioDriver implements AudioOutput2 {
 					if (JavaSoundAudioDriver.this.engineRuntime
 							.getSynchronizer() != null) {
 
-						System.out.println("===> play thread");
-
 						long streamTime = JavaSoundAudioDriver.this.engineRuntime
 								.getSynchronizer().getStreamTime();
 
-						long diff = JavaSoundAudioDriver.this.lastPts
-								- streamTime;
-
-						// FIXME calculate actual play start time
-						// based on index in buffer
-						long sleepTime = diff / 1000;
-
-						// long start = System.currentTimeMillis();
-
-						if (bbuf == null) {
-							bbuf = new byte[(bufferSizeInFrames / 2)
-									* JavaSoundAudioDriver.this.format
-											.getFrameSize()];
+						// LogUtil.debug("lastPts = "
+						// + JavaSoundAudioDriver.this.lastPts);
+						if (LogUtil.isDebugEnabled()) {
+							LogUtil.debug("streamTime=" + streamTime);
 						}
 
-						// JavaSoundAudioDriver.this.bbuf = new
-						// byte[line.available()];
+						long epts = buf.getStartTimestampMillis() * 1000;
+						long vpts = streamTime;
 
-						System.out
-								.println("line available=" + line.available());
-						System.out.println("available in buffer="
-								+ buf.getAvailable());
-						System.out.println("buf space left="
-								+ buf.getSpaceLeft());
+						// System.out
+						// .println("------------ PLAY ------------------");
 
-						// while (buf.getAvailable() < bbuf.length) {
-						// Thread.yield();
-						// }
+						if (LogUtil.isDebugEnabled()) {
+							LogUtil.debug("vpts=" + vpts);
+							LogUtil.debug("epts=" + epts);
+							LogUtil.debug("diff=" + (vpts - epts));
+						}
 
-						len = buf.getAvailable() > bbuf.length ? bbuf.length
-								: buf.getAvailable();
+						byte[] bbuf = null;
 
-						System.out.println("transferring to bbuf: " + len);
+						// if diff < 0 the audio is ahead
+						// else the video is ahead
+
+						long sleepTime = 0;
+						long chunkTime = 500;
 
 						try {
-							System.out.println("off=" + off + " len=" + len);
-							buf.getInputStream().read(bbuf, off, len);
-							// if (len > 0)
-							// off += (len - 1);
-							// if (off == BUFFER_SIZE_IN_BYTES)
-							// off = 0;
+							if (epts >= vpts) {
+								// sleepTime = (epts - vpts) / 1000;
+								continue;
+							} else {
+								long skipMillis = (vpts - epts) / 1000l;
+								if (LogUtil.isDebugEnabled()) {
+									LogUtil.debug("++++++++++++> SKIPPING "
+											+ skipMillis + " millis");
+								}
+
+								if (skipMillis > 0) {
+									int skipped = buf.skipMillis(skipMillis);
+									if (LogUtil.isDebugEnabled()) {
+										LogUtil.debug("actually skipped "
+												+ TimeUtil.audioBytesToMillis(
+														format, skipped));
+										// chunkTime = 0;
+									}
+								}
+							}
+
+							bbuf = buf.readMillis(chunkTime);
+
 						} catch (IOException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
 
-						System.out.println("available in buffer="
-								+ buf.getAvailable());
-						System.out.println("buf space left="
-								+ buf.getSpaceLeft());
-
 						while (!line.isOpen()) {
-							// System.out.println("Line not open");
 							Thread.yield();
 						}
 
 						while (line.isActive()) {
-							// System.out.println("Line is busy");
 							Thread.yield();
 						}
 
-						if (len > 0) {
-							long start = System.currentTimeMillis();
-							// line.start();
-							line.write(bbuf, 0, len);
-							// line.drain();
-							// line.stop();
+						// XXX reset the stream time to the audio's stream
+						// time, and don't sleep (audio stream drives clock)
+						// sleepTime = 0;
+						// ((MultispeedVptsSynchronizer)
+						// JavaSoundAudioDriver.this.engineRuntime
+						// .getSynchronizer()).setStreamTimeZero(epts
+						// - chunkTime, true);
 
-							if (sleepTime > 0) {
+						if (bbuf.length > 0) {
+							if ((sleepTime) > 0) {
 								try {
-									System.out.println(sleepTime);
-									Thread.sleep(0);
+									if (LogUtil.isDebugEnabled()) {
+										LogUtil.debug("----------> SLEEPING "
+												+ (sleepTime));
+									}
+									Thread.sleep(sleepTime);
 								} catch (InterruptedException e) {
 
 								}
 							}
-							long end = System.currentTimeMillis();
-							long audioWriteLatency = end - start;
-							System.out.println("audioWriteLatency="
-									+ audioWriteLatency);
-						}
 
-						System.out.println("<====== end play segment");
+							if (LogUtil.isDebugEnabled()) {
+								LogUtil.debug("writing " + bbuf.length);
+							}
+
+							long start = System.currentTimeMillis();
+							line.write(bbuf, 0, bbuf.length);
+							long end = System.currentTimeMillis();
+
+							audioWriteLatency = (end - start);
+
+							if (LogUtil.isDebugEnabled()) {
+								LogUtil.debug("audioWriteLatency="
+										+ audioWriteLatency);
+							}
+						}
 					}
 				}
 			}
 		});
 
-		// thread.start();
-
+		thread.start();
 	}
 
 	@Override
 	public void setCurrentSamples(IAudioSamples samples) {
 		byte[] arr = new byte[samples.getSize()];
 		samples.get(0, arr, 0, arr.length);
-		//
-		// long timeAlreadyCached = TimeUtil.audioBytesToMillis(this.format, buf
-		// .getAvailable());
-		// System.out.println(timeAlreadyCached + "ms waiting to be played");
+
+		if (firstTimestampInStream < 0) {
+			firstTimestampInStream = samples.getTimeStamp();
+			buf.setStartTimestampMillis(firstTimestampInStream / 1000);
+			buf.setEndTimestampMillis(firstTimestampInStream / 1000);
+
+			// XXX this is done in the decoder
+//			if (!((MultispeedVptsSynchronizer) engineRuntime.getSynchronizer())
+//					.isStreamTimeZeroSet()) {
+//				if (LogUtil.isDebugEnabled())
+//					LogUtil.debug("setting streamTimeZero to "
+//							+ firstTimestampInStream);
+//				((MultispeedVptsSynchronizer) engineRuntime.getSynchronizer())
+//						.setStreamTimeZero(firstTimestampInStream, false);
+//			}
+		}
+
+		// long timeAlreadyCached = buf.getCachedAudioTime();
 		// long aboutToCachePts = samples.getTimeStamp();
-		//
+
 		// this.lastPts = samples.getTimeStamp();
-		//
-		// try {
-		// System.out.println("caching " + samples.getSize() + " bytes");
-		// System.out.println("caching " + samples.getTimeStamp() + " (pts)");
-		// System.out.println("caching "
-		// + TimeUtil.audioBytesToMillis(this.format, arr.length)
-		// + "ms");
-		// long start = System.currentTimeMillis();
-		// buf.getOutputStream().write(arr);
-		// long end = System.currentTimeMillis();
-		// System.out.println("cache took " + (end - start) + " ms");
-		// } catch (IOException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
 
-		if (engineRuntime.getSynchronizer() != null
-				&& engineRuntime.getSynchronizer().getStreamTime() > 0
-				&& !engineRuntime.isPaused()) {
+		try {
+			// LogUtil.debug("++++++++++ CACHE FROM STREAM +++++++++++");
+			// LogUtil.debug(timeAlreadyCached +
+			// "ms waiting to be played");
+			// LogUtil.debug("caching " + samples.getSize() + " bytes");
+			// LogUtil.debug("caching " + samples.getTimeStamp() +
+			// " (pts)");
+			// LogUtil.debug("caching "
+			// + TimeUtil.audioBytesToMillis(this.format, arr.length)
+			// + "ms");
+			// LogUtil.debug("cache space left=" + buf.getSpaceLeft());
+			// LogUtil.debug("+++++++++++++++++++++++++++++++++++++++");
 
-			if (samples.getTimeStamp() > engineRuntime.getSynchronizer()
-					.getStreamTime()) {
-				long diff = (samples.getTimeStamp() - engineRuntime
-						.getSynchronizer().getStreamTime()) / 1000;
+			// long start = System.currentTimeMillis();
+			buf.write(arr);
+			// long end = System.currentTimeMillis();
+			// LogUtil.debug("cache took " + (end - start) + " ms");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-				long time = TimeUtil.audioBytesToMillis(format, arr.length);
-				//System.out.println("time=" + time + " diff=" + diff);
-
-				if (diff > 500) {
-
-					// double the size
-					byte[] origArray = arr;
-					byte[] interpolatedSamples = new byte[origArray.length * 2];
-					samples.get(0, origArray, 0, origArray.length);
-					int frameSize = format.getFrameSize();
-					int frameCount = origArray.length / frameSize;
-					int sampleSizeInBytes = format.getSampleSizeInBits() / 8;
-
-					for (int frame = 0; frame < frameCount; frame++) {
-						int idx = frame * frameSize * 2;
-						int originalFrameIndex = frame * frameSize;
-						interpolatedSamples[idx] = origArray[originalFrameIndex]; // LL
-						interpolatedSamples[idx + 1] = origArray[originalFrameIndex + 1]; // LH
-						interpolatedSamples[idx + 2] = origArray[originalFrameIndex + 2]; // RL
-						interpolatedSamples[idx + 3] = origArray[originalFrameIndex + 3]; // RH
-						interpolatedSamples[idx + 4] = origArray[originalFrameIndex];
-						interpolatedSamples[idx + 5] = origArray[originalFrameIndex + 1];
-						interpolatedSamples[idx + 6] = origArray[originalFrameIndex + 2];
-						interpolatedSamples[idx + 7] = origArray[originalFrameIndex + 3];
-					}
-
-					// if (diff > 0) {
-					// try {
-					// Thread.sleep(diff);
-					// } catch (InterruptedException e) {
-					//
-					// }
-					// }
-
-					line.write(interpolatedSamples, 0,
-							interpolatedSamples.length);
-				} else {
-					line.write(arr, 0, arr.length);
-				}
-			}
+	@Override
+	public void mediaPlayerEventReceived(MediaPlayerEvent evt) {
+		if (LogUtil.isDebugEnabled()) {
+			LogUtil.debug("got event " + evt);
+		}
+		
+		if (evt.getType() == MediaPlayerEvent.Type.SEEK) {
+			buf.clear();
 		}
 	}
 
